5b1b652c47cbc0c070271e4db2fa918fa9c09de5
KatrinSafi <167858422+KatrinSafi@users.noreply.github.com>
2024-10-14 23:09:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+Course = "Python"
+print(Course)
+Total_tasks = 12
+print(Total_tasks)
+Hours_spent = 1.5
+print(Hours_spent)
+Time_for_one_homework = Hours_spent / Total_tasks
+print(Time_for_one_homework)
+print("Course:", Course, "всего задач:",Total_tasks, "затрачено часов:",Hours_spent,"Time_for_one_homework:","среднее время выполнения",Time_for_one_homework, "часа")
